0d59c31a6c56306832e73a4c9655efbb6950c50d
Ashadozzaman <shovoua@gmail.com>
2020-04-08 19:29:58 +0600

    Form validation and fetch data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'handling-form';
+  @ViewChild('f') submitform: NgForm;
+  defaultQuestion = 'pet';
+  answer='';
+  genders = ['male','female'];
+  user ={
+    username: '',
+    email: '',
+    secret: '',
+    gender: '',
+    questionAnswer:''
+  };
+  submitted = false;
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    // this.submitform.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: 'shovoua@gmail.com'
+    //   },
+    //   secret:'pet',
+    //   gender: 'male',
+    //   questionAnswer:''
+    // });
+
+
+    this.submitform.form.patchValue({
+      userData:{
+        username: 'Shvou',
+      }
+    });
+  }
+
+  // onSubmit(form: NgForm){
+  //   console.log(form);
+  // }
+  onSubmit(){
+    this.user.username = this.submitform.value.userData.username,
+    this.user.email = this.submitform.value.userData.email,
+    this.user.secret = this.submitform.value.secret,
+    this.user.gender = this.submitform.value.gender,
+    this.user.questionAnswer = this.submitform.value.questionAnswer
+
+  }
 }
